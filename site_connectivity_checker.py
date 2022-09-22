e9e6a2e8fd372543f8e57f2a571a112f50844fcd
Dann Cuapiaco <cuapiaco.dann@yahoo.com>
2022-09-22 19:00:20 +0800

    edit few lines

--- site_connectivity_checker.py
+++ site_connectivity_checker.py
@@ -1,4 +1,3 @@
-# %%
 import urllib.request as urllib
 
 def main(url):
@@ -12,4 +11,3 @@ print("This is a site connectivity checker program")
 input_url = input("Input the url of the site you want to check: ")
 
 main(input_url)
-# %%
